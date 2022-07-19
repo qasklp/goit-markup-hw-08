0a991e17b89b7dc4b5ebe55bea45cf2670979d12
qasklp <freeee386@gmail.com>
2022-07-20 01:59:52 +0300

    some bugs fixed

--- menu.js
+++ menu.js
@@ -3,12 +3,14 @@
     openMenuBtn: document.querySelector("[data-mobile-menu-open]"),
     closeMenuBtn: document.querySelector("[data-mobile-menu-close]"),
     menu: document.querySelector("[data-mobile-menu]"),
+    bodyNoScroll: document.querySelector("[data-nonScroll]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleModal);
-  refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleModal() {
+  function toggleMenu() {
     refs.menu.classList.toggle("header__menu-backdrop--is-open");
+    refs.bodyNoScroll.classList.toggle("no-scroll")
   }
 })();
